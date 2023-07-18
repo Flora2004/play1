4ee07045a2c97aa06879554ba8803e5d19a99054
angel <1955855131@qq.com>
2023-07-18 13:19:29 +0800

    好的坦克和坏的坦克区分开

--- src/client/Tank.java
+++ src/client/Tank.java
@@ -14,8 +14,8 @@ public class Tank {
     private int x,y;
     private Dir dir;
     private static final int SPEED=5;
-    public static int WIDTH=ResourceMgr.tankU.getWidth();
-    public static int HEIGHT=ResourceMgr.tankU.getHeight();
+    public static int WIDTH=ResourceMgr.goodTankU.getWidth();
+    public static int HEIGHT=ResourceMgr.goodTankU.getHeight();
     private Random random=new Random();
     private boolean moving=true;
     private TankFrame tankFrame;
@@ -74,21 +74,22 @@ public class Tank {
 
 
     public void paint(Graphics g){
-        if(!living) {
-            tankFrame.tanks.remove(this);
-        }
+        if(!living) tankFrame.tanks.remove(this);
+
+
+
         switch (dir){
             case LEFT:
-                g.drawImage(ResourceMgr.tankL,x,y,null);
+                g.drawImage(this.group==Group.GOOD?ResourceMgr.goodTankL:ResourceMgr.badTankL,x,y,null);
                 break;
             case UP:
-                g.drawImage(ResourceMgr.tankU,x,y,null);
+                g.drawImage(this.group==Group.GOOD?ResourceMgr.goodTankU:ResourceMgr.badTankU,x,y,null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR,x,y,null);
+                g.drawImage(this.group==Group.GOOD?ResourceMgr.goodTankR:ResourceMgr.badTankR,x,y,null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD,x,y,null);
+                g.drawImage(this.group==Group.GOOD?ResourceMgr.goodTankD:ResourceMgr.badTankD,x,y,null);
                 break;
         }
 
